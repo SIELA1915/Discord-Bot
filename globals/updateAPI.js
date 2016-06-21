6f0c90a93c64a30d371864941e5604c96caaafa5
Sit Melai <sitmelai@gmail.com>
2016-06-21 21:29:13 +0200

    Fixed ryzom api

--- globals/updateAPI.js
+++ globals/updateAPI.js
@@ -2,19 +2,24 @@ var fs = require("fs");
 
 exports.main = (user, guild) => {
     var request = require("request");
+    var xml2js = require("xml2js");
+    var parser = new xml2js.Parser();
     if (user != null) {
 	var cDB = require("../ressources/ryzomapi/Characters.json");
 	var cKeys = require("../ressources/ryzomapi/Char_Keys.json");
 	for (var u in user) {
 	    var ckey = cKeys[user[u]];
 	    request.post({url:'http://api.ryzom.com/character.php', form: {apikey:ckey}}, function(err,httpResponse,body){
+//		console.log("---------------Error--------------" + err);
+//		console.log("---------------Body---------------" + body);
+//		console.log("---------------Http---------------" + httpResponse);
 		parser.parseString(body, function (err, result) {
+		    console.log(err);
 		    cDB[user[u]] = result;
-		    console.log('Done: ' + user[u]);
+		    fs.writeFileSync(__dirname + "/../ressources/ryzomapi/Characters.json", JSON.stringify(cDB), "utf8");
 		});
 	    });
 	}
-	fs.writeFileSync(__dirname + "/../ressources/ryzomapi/Characters.json", JSON.stringify(cDB), "utf8");
     }
     if (guild) {
 	var gDB = require("../ressources/ryzomapi/Guilds.json");
