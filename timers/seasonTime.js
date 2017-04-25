bf1f27e127dded5a67e627f2126de819987fdcfa
Sit Melai <sitmelai@gmail.com>
2017-04-25 11:48:55 +0200

    finished the 'prep' state to catch offsets happening during the 4 days to next season change

--- timers/seasonTime.js
+++ timers/seasonTime.js
@@ -8,53 +8,86 @@ exports.main = (id, bot, over) => {
 	if (!over && !cT['prep'])
 	    bot.channels.find('id', cT['channel']).sendMessage('Season Change starts now! ' + bot.channels.find('id', cT['channel']).guild.roles.find("id", cT["role"]))
 	    .catch(console.log);
-	setTimeout(function() {
+	if (cT['prep']) {
 	    var nT = cT;
-	    nT['prep']?nT['prep'] = 0:nT['prep'] = 1;
+	    nT['prep'] = 0;
 	    var Atys = require('../ressources/atys/time.js');
 	    var atysDate = new Atys(0);
 	    atysDate.setRLCountdown = (str, rlDate, channel) => {
-		nT['time'] = new Date().getTime() + atysDate.rlMS - (3 * 60 * 60 * 1000);
-		console.log("Season Change is on " + new Date(nT['time']).toUTCString());
-		nT['until'] = 3;
-		nT['untilText'] = 'hours';
+		nT['time'] = new Date().getTime() + atysDate.rlMS;
 		var aTime = timeToGo(new Date(nT['time']));
-		setTimeout(timerFuncs[nT["function"]], (aTime[0] * 60 * 60 * 1000) + (aTime[1] * 60 * 1000) + (aTime[2] * 1000), id, bot, true);
-		timers[timers.size] = nT;
+		setTimeout(timerFuncs[nT["function"]], (aTime[0] * 60 * 60 * 1000) + (aTime[1] * 60 * 1000) + (aTime[2] * 1000), id, bot);
+		timers[id] = nT;
 		var fs = require("fs");
 		fs.writeFileSync(__dirname + "/../ressources/atys/timers.json", JSON.stringify(timers), "utf8");
 		delete require.cache[require.resolve('../ressources/atys/time.js')];
 	    }
-	}, 10 * 60 * 1000);
+	} else {
+	    setTimeout(function() {
+		var nT = cT;
+		nT['prep'] = 1;
+		var Atys = require('../ressources/atys/time.js');
+		var atysDate = new Atys(0);
+		atysDate.setRLCountdown = (str, rlDate, channel) => {
+		    nT['time'] = new Date().getTime() + atysDate.rlMS - (3 * 60 * 60 * 1000);
+		    console.log("Season Change is on " + new Date(nT['time']).toUTCString());
+		    nT['until'] = 3;
+		    nT['untilText'] = 'hours';
+		    var aTime = timeToGo(new Date(nT['time']));
+		    setTimeout(timerFuncs[nT["function"]], (aTime[0] * 60 * 60 * 1000) + (aTime[1] * 60 * 1000) + (aTime[2] * 1000), id, bot);
+		    timers[id] = nT;
+		    var fs = require("fs");
+		    fs.writeFileSync(__dirname + "/../ressources/atys/timers.json", JSON.stringify(timers), "utf8");
+		    delete require.cache[require.resolve('../ressources/atys/time.js')];
+		}
+	    }, 10 * 60 * 1000);
+	}
     } else {
-	if (!over)
+	if (!over && !cT['prep'])
 	    bot.channels.find("id", cT["channel"]).sendMessage('Season Change in ' + cT['until'] + ' ' + cT['untilText'] + '! ' + bot.channels.find('id', cT['channel']).guild.roles.find("id", cT["role"]))
 	    .catch(console.log);
-	var nT = cT;
-	if (nT['until'] == 1)
-	    nT['until'] = 30;
-	else if (nT['until'] == 30)
-	    nT['until'] = 0;
-	else
-	    nT['until']--;
-	
-	if (nT['until'] == 1)
-	    nT['untilText'] = 'hour';
-	else if (nT['until'] == 30)
+	if (cT['prep']) {
+	    var nT = cT;
+	    nT['prep'] = 0;
+	    var Atys = require('../ressources/atys/time.js');
+	    var atysDate = new Atys(0);
+	    atysDate.setRLCountdown = (str, rlDate, channel) => {
+		if (nT['until'] == 30)
+		    nT['time'] = new Date().getTime() + atysDate.rlMS - (30 * 60 * 1000);
+		else
+		    nT['time'] = new Date().getTime() + atysDate.rlMS - (nT['until'] * 60 * 60 * 1000);
+		var aTime = timeToGo(new Date(nT['time']));
+		setTimeout(timerFuncs[nT["function"]], (aTime[0] * 60 * 60 * 1000) + (aTime[1] * 60 * 1000) + (aTime[2] * 1000), id, bot);
+		timers[id] = nT;
+		var fs = require("fs");
+		fs.writeFileSync(__dirname + "/../ressources/atys/timers.json", JSON.stringify(timers), "utf8");
+		delete require.cache[require.resolve('../ressources/atys/time.js')];
+	    }
+	} else {
+	    var nT = cT;
+	    nT['prep'] = 1;
+	    if (nT['until'] == 1)
+		nT['until'] = 30;
+	    else if (nT['until'] == 30)
+		nT['until'] = 0;
+	    else
+		nT['until']--;
+	    
+	    if (nT['until'] == 1)
+		nT['untilText'] = 'hour';
+	    else if (nT['until'] == 30)
 	    nT['untilText'] = 'minutes';
-	if (nT['until'] == 30)
-	    nT['time'] = nT['time'] + 30 * 60 * 1000;
-	else if (nT['until'] == 0)
-	    nT['time'] = nT['time'] + 30 * 60 * 1000;
-	else
-	    nT['time'] = nT['time'] + 60 * 60 * 1000;
-	var aTime = timeToGo(new Date(nT["time"]));
-	if (aTime[3] < 0)
-	    setTimeout(timerFuncs[nT["function"]], 2, id, bot, true);
-	else
+	    if (nT['until'] == 30)
+		nT['time'] = nT['time'] + 30 * 60 * 1000;
+	    else if (nT['until'] == 0)
+		nT['time'] = nT['time'] + 30 * 60 * 1000;
+	    else
+		nT['time'] = nT['time'] + 60 * 60 * 1000;
+	    var aTime = timeToGo(new Date(nT["time"]));
 	    setTimeout(timerFuncs[nT["function"]], (aTime[0] * 60 * 60 * 1000) + (aTime[1] * 60 * 1000) + (aTime[2] * 1000), id, bot);
-	timers[timers.size] = nT;
-    var fs = require("fs");
-    fs.writeFileSync(__dirname + "/../ressources/atys/timers.json", JSON.stringify(timers), "utf8");
+	    timers[id] = nT;
+	    var fs = require("fs");
+	    fs.writeFileSync(__dirname + "/../ressources/atys/timers.json", JSON.stringify(timers), "utf8");
+	}
     }
 }
